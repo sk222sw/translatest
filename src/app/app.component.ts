0747b5fe6057b777275548a3babe82d168d8b0bc
Sonny Kjellberg <sonny.kjellberg@cab.se>
2018-01-08 10:17:30 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  param;
+  language;
+  translate;
+  translationParams = ['WORLD'];
+
+  constructor(translate: TranslateService) {
+    this.translate = translate;
+
+    translate.addLangs(['en', 'sv']);
+    translate.setDefaultLang('en');
+
+    this.language = translate.getBrowserLang();
+
+    this.setupLanguage();
+  }
+
+  setupLanguage() {
+    this.translate.use(this.language);
+
+    this.translate.get('WORLD').subscribe(res => {
+      this.param = {value: res};
+    });
+  }
+
+  changeLanguage() {
+    this.language = this.language === 'en' ? 'sv' : 'en';
+    this.setupLanguage();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
